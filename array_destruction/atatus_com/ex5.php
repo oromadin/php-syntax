cc11ea7e4048b61792dab74901da7b35f0c2230e
Oleg Romadin <osromadin@gmail.com>
2024-10-24 19:11:22 +0300

    add ex5.php

---
+++ array_destruction/atatus_com/ex5.php
@@ -0,0 +1,38 @@
+<?php
+
+//Swapping variable values using array destructuring
+
+$customer = [
+  'name' => 'John Doe',
+  'email' => 'johndoe@example.com',
+  'phone' => '555-1234',
+];
+
+echo "Customer data before swap: \n";
+print_r($customer);
+
+[
+	$customer['email'], 
+	$customer['phone']
+] = [
+	$customer['phone'], 
+	$customer['email']
+];
+
+echo "Customer data after swap: \n";
+print_r($customer);
+
+// Customer data before swap:
+// Array
+// (
+//     [name] => John Doe
+//     [email] => johndoe@example.com
+//     [phone] => 555-1234
+// )
+// Customer data after swap:
+// Array
+// (
+//     [name] => John Doe
+//     [email] => 555-1234
+//     [phone] => johndoe@example.com
+// )
