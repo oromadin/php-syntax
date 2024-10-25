cff6f9d91312992697f49ff6680be66163a9c058
Oleg Romadin <osromadin@gmail.com>
2024-10-25 14:54:02 +0300

    array destructuring

--- array_destruction/atatus_com/ex10.php
+++ array_destruction/atatus_com/ex10.php
@@ -1,19 +1,5 @@
 <?php
 
-// Undefined or missing array keys
-
-$data = ['name' => 'John Doe'];
-//[$name, $email] = $data;  // PHP Notice: Undefined offset: 1
-
-// Проверяем есть ли ключ
-$data = ['name' => 'John Doe'];
-[$name, $email] = $data + [null, null];
-
-var_dump($name); // null
-var_dump($email); // null
-
-// Incorrect array structures
-
 $data = ['John Doe', 'john@example.com'];
 
 [$name, $email, $phone] = $data;
