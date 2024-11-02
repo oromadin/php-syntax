fedb6cb3b7e78fef04c774641b3b722aeb2619d0
oromadin <osromadin@gmail.com>
2024-11-02 17:46:17 +0000

    fix code style

--- array_any_array_all/laravel_news_com/ex4.php
+++ array_any_array_all/laravel_news_com/ex4.php
@@ -1,4 +1,4 @@
 <?php
 
 // Check, if all animal names are longer than 5 letters.
-return array_all(fn($name) => strlen($name) > 5, ANIMALS);
+return array_all(fn ($name) => strlen($name) > 5, ANIMALS);
--- array_any_array_all/lib/array_all.php
+++ array_any_array_all/lib/array_all.php
@@ -3,7 +3,8 @@
 /**
  * Returns true if the given predicate is true for all elements.
  */
-function array_all(callable $callback, array $items) {
+function array_all(callable $callback, array $items)
+{
     foreach ($items as $item) {
         if(!$callback($item)) {
             return false;
