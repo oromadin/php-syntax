f78ea005e999cd29c2549a4a4ae7ef61557de922
oromadin <osromadin@gmail.com>
2024-12-16 16:06:36 +0200

    add example with array

--- array_column/array_column.php
+++ array_column/array_column.php
@@ -1,2 +1,39 @@
 <?php
 
+$records = array(
+    array(
+        'id' => 2135,
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+    ),
+    array(
+        'id' => 3245,
+        'first_name' => 'Sally',
+        'last_name' => 'Smith',
+    ),
+    array(
+        'id' => 5342,
+        'first_name' => 'Jane',
+        'last_name' => 'Jones',
+    ),
+    array(
+        'id' => 5623,
+        'first_name' => 'Peter',
+        'last_name' => 'Doe',
+    )
+);
+
+$first_names = array_column($records, 'first_name');
+echo "<pre>";
+print_r($first_names);
+echo "</pre>";
+
+$first_names = array_column($records, null, 'id');
+echo "<pre>";
+print_r($first_names);
+echo "</pre>";
+
+$last_names = array_column($records, 'last_name', 'first_name');
+echo "<pre>";
+print_r($last_names);
+echo "</pre>";
